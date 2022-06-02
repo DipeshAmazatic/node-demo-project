0fd719e313951a5d970a66e34baf9a880402cf88
DipeshAmazatic <94514763+DipeshAmazatic@users.noreply.github.com>
2022-06-02 19:16:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
-const port = process.env.POR || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
